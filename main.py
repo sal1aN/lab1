ccc32870b4dcc3f2a8fad1d672f2dc2f7c930ac3
sal1aN <popovicevgen1@gmail.com>
2026-09-10 11:12:55 +0300

    added error handling

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def f_to_c():
     proceed = True
     while proceed:
-        temp = input("Enter temperature in F: ")
-        temp = int(temp)
-        tempC = (temp - 32) * 5/9
+        try:
+            temp = input("Enter temperature in F: ")
+            temp = int(temp)
+            tempC = (temp - 32) * 5/9
+        except ValueError:
+            print("Please enter a valid temperature")
+            continue
         print(f"Temperature in C: {tempC:.1f}")
         print("Do you wish to continue? (y/n)")
         choice = input()
