a4043580e9da07bd1ba71f419c4bb9399d42a869
sal1aN <popovicevgen1@gmail.com>
2026-09-10 11:09:21 +0300

    added f_to_c function

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+def f_to_c():
+    proceed = True
+    while proceed:
+        temp = input("Enter temperature in F: ")
+        temp = int(temp)
+        tempC = (temp - 32) * 5/9
+        print(f"Temperature in C: {tempC:.1f}")
+        print("Do you wish to continue? (y/n)")
+        choice = input()
+        if choice == "y":
+            proceed = True
+        else:
+            proceed = False
+    return
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    f_to_c()
