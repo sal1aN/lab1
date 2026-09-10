03442a0bca397e6200d1ddfc3f1e67e0384aa0a3
sal1aN <popovicevgen1@gmail.com>
2026-09-10 11:20:33 +0300

    refactored code

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-def f_to_c():
+def convert_f_to_C(tempF):
+    try:
+        tempF = int(tempF)
+        tempC = (tempF - 32) * 5 / 9
+    except ValueError:
+        print("Please enter a valid temperature")
+        return False
+    return tempC
+
+def run():
     proceed = True
     while proceed:
-        try:
-            temp = input("Enter temperature in F: ")
-            temp = int(temp)
-            tempC = (temp - 32) * 5/9
-        except ValueError:
-            print("Please enter a valid temperature")
-            continue
-        print(f"Temperature in C: {tempC:.1f}")
-        print("Do you wish to continue? (y/n)")
-        choice = input()
-        if choice == "y":
-            proceed = True
-        else:
-            proceed = False
+        temp = input("Enter temperature in F: ")
+        tempC = convert_f_to_C(temp)
+        if tempC:
+            print(f"Temperature in C: {tempC:.1f}")
+            print("Do you wish to continue? (y/n)")
+            choice = input()
+            if choice == "y":
+                proceed = True
+            else:
+                proceed = False
     return
 
 if __name__ == '__main__':
-    f_to_c()
+    run()
